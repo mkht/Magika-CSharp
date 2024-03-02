714f4bc623d9b99c09234195a7249594f6d6130a
mkht <mkht@users.noreply.github.com>
2024-03-02 17:29:05 +0900

    Update build script

--- build.ps1
+++ build.ps1
@@ -1,43 +1,90 @@
+
+$OutputDir = (Join-Path $PSScriptRoot '/Release')
+Remove-Item $OutputDir -Recurse -Force -ErrorAction Ignore
+
 Push-Location -Path (Join-Path $PSScriptRoot '/magika')
+
+Remove-Item ./bin -Recurse -Force -ErrorAction Ignore
+Remove-Item ./obj -Recurse -Force -ErrorAction Ignore
+
 dotnet restore
 dotnet clean
 
 # Build and publish for class library
-## for Windows (Legacy .NET Framework)
-dotnet publish -c Release -f netstandard2.0
+$name = 'magika-lib'
 
-## for Windows (.NET Core)
-dotnet publish -c Release -f net6.0 -r win-x64
-dotnet publish -c Release -f net6.0 -r win-arm64
+# win-x64
+$arch = 'win-x64'
+dotnet publish -c Release -f netstandard2.0 -r $arch -o ("$OutputDir/$name-$arch/netstandard2.0")
+dotnet publish -c Release -f net6.0 -r $arch -o ("$OutputDir/$name-$arch/net6.0")
 
-## for Linux (.NET Core)
-dotnet publish -c Release -f net6.0 -r linux-x64
-dotnet publish -c Release -f net6.0 -r linux-arm64
+# win-arm64
+$arch = 'win-arm64'
+dotnet publish -c Release -f net6.0 -r $arch -o ("$OutputDir/$name-$arch/net6.0")
 
-## for macOS (.NET Core)
-dotnet publish -c Release -f net6.0 -r osx-x64
-dotnet publish -c Release -f net6.0 -r osx-arm64
+# linux-x64
+$arch = 'linux-x64'
+dotnet publish -c Release -f net6.0 -r $arch -o ("$OutputDir/$name-$arch/net6.0")
+Remove-Item "$OutputDir/$name-$arch/net6.0/onnxruntime*.dll" -Force
 
-## NuGet package
-dotnet pack -c Release
+# linux-arm64
+$arch = 'linux-arm64'
+dotnet publish -c Release -f net6.0 -r $arch -o ("$OutputDir/$name-$arch/net6.0")
+Remove-Item "$OutputDir/$name-$arch/net6.0/onnxruntime*.dll" -Force
+
+# osx-x64
+$arch = 'osx-x64'
+dotnet publish -c Release -f net6.0 -r $arch -o ("$OutputDir/$name-$arch/net6.0")
+Remove-Item "$OutputDir/$name-$arch/net6.0/onnxruntime*.dll" -Force
+
+# osx-arm64
+$arch = 'osx-arm64'
+dotnet publish -c Release -f net6.0 -r $arch -o ("$OutputDir/$name-$arch/net6.0")
+Remove-Item "$OutputDir/$name-$arch/net6.0/onnxruntime*.dll" -Force
+
+# NuGet package
+dotnet pack -c Release -o $OutputDir
 
 
 # Build and publish for console application
 Push-Location "./cli"
+Remove-Item ./bin -Recurse -Force -ErrorAction Ignore
+Remove-Item ./obj -Recurse -Force -ErrorAction Ignore
+
 dotnet restore
 dotnet clean
 
-## for Windows
-dotnet publish -c Release -f net8.0 -r win-x64
-dotnet publish -c Release -f net8.0 -r win-arm64
+$name = 'magika-cli'
 
-## for Linux
-dotnet publish -c Release -f net8.0 -r linux-x64
-dotnet publish -c Release -f net8.0 -r linux-arm64
+# win-x64
+$arch = 'win-x64'
+dotnet publish -c Release -f net8.0 -r $arch -o ("$OutputDir/$name-$arch")
+Remove-Item ("$OutputDir/$name-$arch/*") -Exclude "magika.exe" -Force -Recurse
 
-## for macOS
-dotnet publish -c Release -f net8.0 -r osx-x64
-dotnet publish -c Release -f net8.0 -r osx-arm64
+# win-arm64
+$arch = 'win-arm64'
+dotnet publish -c Release -f net8.0 -r $arch -o ("$OutputDir/$name-$arch")
+Remove-Item ("$OutputDir/$name-$arch/*") -Exclude "magika.exe" -Force -Recurse
+
+# linux-x64
+$arch = 'linux-x64'
+dotnet publish -c Release -f net8.0 -r $arch -o ("$OutputDir/$name-$arch")
+Remove-Item ("$OutputDir/$name-$arch/*") -Exclude "magika" -Force -Recurse
+
+# linux-arm64
+$arch = 'linux-arm64'
+dotnet publish -c Release -f net8.0 -r $arch -o ("$OutputDir/$name-$arch")
+Remove-Item ("$OutputDir/$name-$arch/*") -Exclude "magika" -Force -Recurse
+
+# osx-x64
+$arch = 'osx-x64'
+dotnet publish -c Release -f net8.0 -r $arch -o ("$OutputDir/$name-$arch")
+Remove-Item ("$OutputDir/$name-$arch/*") -Exclude "magika" -Force -Recurse
+
+# osx-arm64
+$arch = 'osx-arm64'
+dotnet publish -c Release -f net8.0 -r $arch -o ("$OutputDir/$name-$arch")
+Remove-Item ("$OutputDir/$name-$arch/*") -Exclude "magika" -Force -Recurse
 
 Pop-Location
 Pop-Location
